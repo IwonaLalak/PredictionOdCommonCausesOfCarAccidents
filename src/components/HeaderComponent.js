43b66e7f0b68360273c849a57be853a4e5c0a2c8
iwona Lalak <lalak.iwona@gmail.com>
2019-01-24 12:24:44 +0100

    initial commit, change description

---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+export default class HeaderComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
+
+    componentDidMount() {
+    }
+
+
+
+    render() {
+
+        return (
+            <div>
+                header
+            </div>
+        );
+    }
+}
+
