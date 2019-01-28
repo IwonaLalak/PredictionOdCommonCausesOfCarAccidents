08713d17b2ea460acb61e476d876b85e958458eb
iwona Lalak <lalak.iwona@gmail.com>
2019-01-28 12:25:15 +0100

    about author, header component content

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,9 +1,12 @@
 import React, {Component} from 'react';
+import {Button, Col, Modal, ModalBody, ModalFooter, ModalHeader, Row} from "reactstrap";
 
 export default class HeaderComponent extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            modalVisible: false
+        };
     }
 
     componentDidMount() {
@@ -13,16 +16,63 @@ export default class HeaderComponent extends Component {
     render() {
 
         return (
-            <div id={'HEADER_COMPONENT'}>
-                <div id={'header_bar'}>
-                    <h4>
-                        <i className={'fa fa-user-circle-o'}></i> About author
-                    </h4>
-                    <div className={'clear'}></div>
+            <div>
+                <div id={'HEADER_COMPONENT'}>
+                    <div id={'header_bar'}>
+                        <h4 onClick={() => {
+                            this.setState({modalVisible: !this.state.modalVisible})
+                        }}>
+                            <i className={'fa fa-user-circle-o'}></i> About author
+                        </h4>
+                        <div className={'clear'}></div>
+                    </div>
+                    <div id={'header_main_component'}>
+                        <div id={'inner-header-main-component-icon'}>
+                            <i className={'fa fa-object-group'}></i>
+                        </div>
+                        <div id={'inner-header-main-component'}>
+                            <div>
+                                <h1>
+                                    Prediction of common causes of car accidents
+                                </h1>
+                            </div>
+                            <div>
+                                <h2>
+                                    A project aimed at using machine learning to predict the most common causes of road accidents
+                                </h2>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div id={'header_main_component'}>
-
-                </div>
+                <Modal isOpen={this.state.modalVisible} toggle={()=>{this.setState({modalVisible: !this.state.modalVisible})}} className={this.props.className}>
+                    <ModalHeader toggle={()=>{this.setState({modalVisible: !this.state.modalVisible})}}>About author</ModalHeader>
+                    <ModalBody>
+                        <div id={'about-author-modal'}>
+                            <Row>
+                                <Col xs={12}>
+                                    <div>
+                                        <label><i className={'fa fa-user-circle-o'}></i> Author:</label> <span>Iwona Lalak</span>
+                                    </div>
+                                    <div>
+                                        <label><i className={'fa fa-map-marker'}></i> Place:</label> <span>Rzeszów, Poland</span>
+                                    </div>
+                                    <div>
+                                        <label><i className={'fa fa-envelope'}></i> Email:</label> <span>lalak.iwona@gmail.com</span>
+                                    </div>
+                                    <div>
+                                        <label><i className={'fa fa-linkedin-square'}></i> LinkedIn:</label> <a href={'https://www.linkedin.com/in/iwona-lalak-461b52142/'} target={'_blank'}>/IwonaLalak</a>
+                                    </div>
+                                    <div>
+                                        <label><i className={'fa fa-github'}></i> GitHub:</label> <a href={'https://github.com/IwonaLalak'} target={'_blank'}>/IwonaLalak</a>
+                                    </div>
+                                </Col>
+                            </Row>
+                        </div>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color="secondary" onClick={()=>{this.setState({modalVisible: !this.state.modalVisible})}}>Close</Button>
+                    </ModalFooter>
+                </Modal>
             </div>
         );
     }
