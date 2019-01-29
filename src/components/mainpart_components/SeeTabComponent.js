f5c276a8c5e804a4d5ab69c587dcaf5bfb585ef1
iwona Lalak <lalak.iwona@gmail.com>
2019-01-29 13:06:04 +0100

    load data, data

--- src/components/mainpart_components/SeeTabComponent.js
+++ src/components/mainpart_components/SeeTabComponent.js
@@ -1,23 +1,60 @@
 import React, {Component} from 'react';
-import {Row, Col} from "reactstrap";
+import {Row, Col, Button, Spinner} from "reactstrap";
+import DataTable from "./seetab_components/DataTable";
 
 export default class SeeTabComponent extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            data: [],
+            setLoader: false
+        };
     }
 
     componentDidMount() {
+
     }
 
+
+    loadDataIntoState() {
+        this.setState({setLoader: true})
+        import('../../data/UM_2015_6dot5k.js').then(function (response) {
+            this.setState({
+                data: response.default.data,
+                setLoader: false
+            })
+        }.bind(this))
+    }
 
     render() {
 
         return (
             <div className={'tab_container'}>
                 <Row>
+                    {
+                        (!this.state.data.length > 0 && this.state.setLoader === false) ?
+                            <Col xs={12}>
+                                <span style={{marginRight: '5px'}}>Please load data first</span>
+                                <Button color={'primary'} onClick={() => this.loadDataIntoState()}>Load Data</Button>
+                            </Col>
+                            :
+                            ''
+
+                    }
                     <Col xs={12}>
-                        See this tab component
+                        {
+                            (this.state.data.length === 0) ?
+                                (this.state.setLoader) ?
+                                    <span><Spinner color={'primary'}/> Loading...</span>
+                                    :
+                                    ''
+                                :
+                                <DataTable data={this.state.data}/>
+                        }
+                    </Col>
+                    <Col xs={12}>
+
+
                     </Col>
                 </Row>
             </div>
---
+++ src/components/mainpart_components/seetab_components/DataTable.js
@@ -0,0 +1,102 @@
+import React, {Component} from 'react';
+import {BootstrapTable, TableHeaderColumn} from "react-bootstrap-table";
+import tabgrid from "../../../utilities/Tabgrid";
+
+export default class DataTable extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            quantity: 0
+        };
+    }
+
+    componentDidMount() {
+
+    }
+
+
+    afterColumnFilter(column, records) {
+        document.getElementById('quantity-of-table').innerText = records.length
+    }
+
+    render() {
+
+        const options = {
+            afterColumnFilter: (column, records) => this.afterColumnFilter(column, records),
+        };
+
+        return (
+            <div>
+                Quantity: <span id={'quantity-of-table'}>{this.props.data.length}</span>
+                <div>
+                    <BootstrapTable
+                                    data={this.props.data}
+                                    pagination
+                                    hover
+                                    options={options}
+                    >
+                        <TableHeaderColumn dataField='caseVehicleId' isKey
+                                           hidden={true}
+                        >id</TableHeaderColumn>
+                        <TableHeaderColumn dataField='vehicleBodyType'
+                                           filter={{type: 'TextFilter', delay: 500, placeholder: 'Szukaj'}}
+                                           thStyle={tabgrid.tg2} tdStyle={tabgrid.tg2}
+                        >
+                            type
+                        </TableHeaderColumn>
+                        <TableHeaderColumn dataField='vehicleBrand'
+                                           filter={{type: 'TextFilter', delay: 500, placeholder: 'Szukaj'}}
+                                           thStyle={tabgrid.tg1} tdStyle={tabgrid.tg1}
+                        >
+                            brand
+                        </TableHeaderColumn>
+                        <TableHeaderColumn dataField='verhicleYear'
+                                           filter={{type: 'TextFilter', delay: 500, placeholder: 'Szukaj'}}
+                                           thStyle={tabgrid.tg1} tdStyle={tabgrid.tg1}
+                        >
+                            vehicle year
+                        </TableHeaderColumn>
+                        <TableHeaderColumn dataField='actionPriorToAccident'
+                                           filter={{type: 'TextFilter', delay: 500, placeholder: 'Szukaj'}}
+                                           thStyle={tabgrid.tg3} tdStyle={tabgrid.tg3}
+                        >
+                            action prior to accident
+                        </TableHeaderColumn>
+                        <TableHeaderColumn dataField='numberOfOccupants'
+                                           filter={{type: 'TextFilter', delay: 500, placeholder: 'Szukaj'}}
+                                           thStyle={tabgrid.tg1} tdStyle={tabgrid.tg1}
+                        >
+                            number of occupants
+                        </TableHeaderColumn>
+                        <TableHeaderColumn dataField='contributingFactor'
+                                           filter={{type: 'TextFilter', delay: 500, placeholder: 'Szukaj'}}
+                                           thStyle={tabgrid.tg1} tdStyle={tabgrid.tg1}
+                        >
+                            contributing factor
+                        </TableHeaderColumn>
+                        <TableHeaderColumn dataField='contributingFactorDescription'
+                                           filter={{type: 'TextFilter', delay: 500, placeholder: 'Szukaj'}}
+                                           thStyle={tabgrid.tg3} tdStyle={tabgrid.tg3}
+                        >
+                            contributing factor desc
+                        </TableHeaderColumn>
+                        <TableHeaderColumn dataField='otherContrubutingFactor'
+                                           filter={{type: 'TextFilter', delay: 500, placeholder: 'Szukaj'}}
+                                           thStyle={tabgrid.tg1} tdStyle={tabgrid.tg1}
+                        >
+                            other contributing factor
+                        </TableHeaderColumn>
+                        <TableHeaderColumn dataField='otherContributingFactorDescription'
+                                           filter={{type: 'TextFilter', delay: 500, placeholder: 'Szukaj'}}
+                                           thStyle={tabgrid.tg3} tdStyle={tabgrid.tg3}
+                        >
+                            other contributing factor desc
+                        </TableHeaderColumn>
+                    </BootstrapTable>
+
+                </div>
+            </div>
+        );
+    }
+}
+
